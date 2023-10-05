f01f4e6b8022c1eff161eed5097c29758a469d6e
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-10-04 23:37:11 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_07_SMN_7_ACT_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Ingrese el valor de n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double suma = 0;
+for (int i = 1; i <= n; i++)
+{
+    double termino = (16 - i) / (3.0 * i); 
+    Console.WriteLine($"Resultado: {termino}");
+    suma += termino;
+}
+Console.WriteLine("=================================");
+Console.WriteLine("Total : "+ suma);
